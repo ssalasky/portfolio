121f7be172165487c1d768065d88226aa64b3ee6
ssalasky <samsalasky@hotmail.com>
2023-04-17 11:56:55 -0600

    main: packages added and skeleton of side created

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Suspense } from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
-function App() {
+import Header from './components/Header';
+import About from './pages/about';
+import Home from './pages/home';
+import Portfolio from './pages/portfolio';
+import Resume from './pages/resume';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<div />}>
+      <Router>
+        <Header />
+        <Routes>
+          <Route element={<Home />} path="/" />
+          <Route element={<About />} path="/about" />
+          <Route element={<Resume />} path="/resume" />
+          <Route element={<Portfolio />} path="/portfolio" />
+        </Routes>
+      </Router>
+    </Suspense>
   );
-}
+};
 
 export default App;
